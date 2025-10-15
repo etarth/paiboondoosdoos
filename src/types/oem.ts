2ece5d4ce18c3ed12fa2f0c041603123180dec14
Patchanon51428 <patchanon.chung@gmail.com>
2025-10-15 16:15:18 +0700

    Comparepage

---
+++ src/types/oem.ts
@@ -0,0 +1,15 @@
+export interface OEM {
+  id: string;
+  name: string;
+  categories: string[];
+  country: string;
+  region: string;
+  rating: number;
+  website: string;
+  priceRange: string;
+  deliveryTime: string;
+  moq: string;
+  certifications: string[];
+  reliability: number;
+  description: string;
+}
